9bb373b2a9f764cee280431cd4c467add21f2a59
Metehan Goksel Kurtulan <metehankurtulan@hotmail.com>
2025-10-14 02:11:39 +0300

    Custom GA Editor for tag generation

---
+++ PWGasCore/Source/PWGASCoreEditor/Private/PWGASCoreEditor.cpp
@@ -0,0 +1,21 @@
+﻿#include "PWGASCoreEditor.h"
+
+#include "PWGA_DetailCustomization.h"
+
+#define LOCTEXT_NAMESPACE "FPWGASCoreEditorModule"
+
+void FPWGASCoreEditorModule::StartupModule()
+{
+	FPropertyEditorModule& Prop = FModuleManager::LoadModuleChecked<FPropertyEditorModule>("PropertyEditor");
+	Prop.RegisterCustomClassLayout(TEXT("PWGameplayAbilityBase"),
+		FOnGetDetailCustomizationInstance::CreateStatic(&FPWGA_DetailCustomization::MakeInstance));
+}
+
+void FPWGASCoreEditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FPWGASCoreEditorModule, PWGASCoreEditor)
---
+++ PWGasCore/Source/PWGASCoreEditor/Private/PWGA_DetailCustomization.cpp
@@ -0,0 +1,40 @@
+﻿// copyright mkurtt96
+
+
+#include "PWGA_DetailCustomization.h"
+
+#include "DetailCategoryBuilder.h"
+#include "DetailLayoutBuilder.h"
+#include "DetailWidgetRow.h"
+#include "GAS/Abilities/PWGameplayAbilityBase.h"
+
+TSharedRef<IDetailCustomization> FPWGA_DetailCustomization::MakeInstance()
+{
+	return MakeShareable(new FPWGA_DetailCustomization);
+}
+
+void FPWGA_DetailCustomization::CustomizeDetails(IDetailLayoutBuilder& DetailBuilder)
+{
+	DetailBuilder.GetObjectsBeingCustomized(SelectedObjects);
+
+	IDetailCategoryBuilder& Cat = DetailBuilder.EditCategory("Tags");
+	Cat.AddCustomRow(FText::FromString("Generate Ability Tags"))
+	.WholeRowContent()
+	[
+		SNew(SButton)
+		.Text(FText::FromString("Generate Ability Tags And Assign Defaults"))
+		.OnClicked(FOnClicked::CreateSP(this, &FPWGA_DetailCustomization::OnGenerateTagsClicked))
+	];
+}
+
+FReply FPWGA_DetailCustomization::OnGenerateTagsClicked()
+{
+	for (TWeakObjectPtr<UObject> Obj : SelectedObjects)
+	{
+		if (UPWGameplayAbilityBase* GA = Cast<UPWGameplayAbilityBase>(Obj.Get()))
+		{
+			GA->GenerateIdentityTag();
+		}
+	}
+	return FReply::Handled();
+}
